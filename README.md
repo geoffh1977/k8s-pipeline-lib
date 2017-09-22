caed4c20f74e597f5952bb91a8a96535152e037c
Geoffrey Harrison <Geoff.Harrison@bulletproof.net>
2017-09-22 19:00:59 +1000

    Added Markdown and Moved Files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Jenkins Kubernetes Groovy Pipeline Library
+
+## Description
+A simple groovy file which contains functions for use in other projects.
--- src/org/pipeline/Pipeline.groovy
+++ src/org/pipeline/Pipeline.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/groovy
-package local.kube;
+package org.pipeline;
 
 def kubectlTest() {
     // Test that kubectl can correctly communication with the Kubernetes API
