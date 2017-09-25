6ecddc202f3578cadfa2ea819733ddf1d012b14d
Geoffrey Harrison <Geoff.Harrison@bulletproof.net>
2017-09-25 13:18:51 +1000

    Try Fixing Docker Push With Loop

--- src/org/pipeline/Pipeline.groovy
+++ src/org/pipeline/Pipeline.groovy
@@ -101,16 +101,17 @@ def containerBuildPub(Map args) {
 
     println "Running Docker Build/Publish: ${args.host}/${args.acct}/${args.repo}:${args.tags}"
 
-    println "Test SH Line"
-    sh 'echo Test Line'
-
     docker.withRegistry("https://${args.host}", "${args.auth_id}") {
 
         // def img = docker.build("${args.acct}/${args.repo}", args.dockerfile)
-        def img = docker.image("${args.acct}/${args.repo}")
+        //def img = docker.image("${args.acct}/${args.repo}")
         sh "docker build --build-arg VCS_REF=${env.GIT_SHA} --build-arg BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'` -t ${args.acct}/${args.repo} ${args.dockerfile}"
+        def img = docker.image("${args.acct}/${args.repo}")
+
         for (int i = 0; i < args.tags.size(); i++) {
-            img.push(args.tags.get(i))
+            //img.push(args.tags.get(i))
+            sh "docker tag ${args.acct}/${args.repo} ${args.host}/${args.acct}/${args.repo}:${args.tags.get(i)}"
+            sh "docker push ${args.host}/${args.acct}/${args.repo}:${args.tags.get(i)}"
         }
 
         return img.id
